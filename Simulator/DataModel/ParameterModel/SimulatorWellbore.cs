1f83ed2245d09acfc23eb56d5b504debcb78469a
Lucas Volpi <lvol@norceresearch.no>
2026-03-16 14:26:55 +0100

    Corrected casing section calculations to include open hole sections

--- Simulator/DataModel/ParameterModel/SimulatorWellbore.cs
+++ Simulator/DataModel/ParameterModel/SimulatorWellbore.cs
@@ -11,97 +11,73 @@ namespace NORCE.Drilling.Simulator4nDOF.Simulator.DataModel.ParametersModel
         public readonly double WallDamping = 5.0e4;                   // percent of mass imbalance compared to total mass
 
         // Geometry to be configured
-        private List<SimulatorBoreHole> boreHoleSizes {get; set;}
+        private List<SimulatorBoreHole> BoreHoleSizes {get; set;}
         public Vector<double> DrillStringClearance;            // [m] drillstring radial clearance to the borehole wall
 
         private Vector<double> boreholeRadius;                   // [m] Wellbore radius calculation
 
-        public SimulatorWellbore(in SimulatorDrillString drillString,
+        public SimulatorWellbore(
+                        in SimulatorDrillString drillString,
                         in LumpedCells lumpedCells,
-                        ModelShared.CasingSection casingSection
-                        )
+                        in CasingSection casingSection
+                    )
         {
             
             List<SimulatorBoreHole> boreHoleSizes = new();
             double depth = casingSection.TopDepth.GaussianValue.Mean ?? 0;
             foreach (BoreHoleSize boreHoleSize in casingSection.CasingSectionSizeTable)
             {
-                boreHoleSizes.Add(new SimulatorBoreHole{
+                boreHoleSizes.Add(
+                    new SimulatorBoreHole
+                    {
                         Depth = (double ) depth,
                         Diameter = (double) boreHoleSize.HoleSize.GaussianValue.Mean!,
                         Length  = (double) boreHoleSize.Length.GaussianValue.Mean!  
                     }
                 );
                 depth += boreHoleSize.Length.GaussianValue.Mean ?? 0.0;
-
-
             }
-
-
-
-
-            depth = 0;
-            if(casingSection.TopDepth.GaussianValue.Mean != null)
+            if (casingSection.OpenHoleSection != null)
             {
-                depth += (double) casingSection.TopDepth.GaussianValue.Mean;
+                foreach (BoreHoleSize boreHoleSize in casingSection.OpenHoleSection.HoleSizes)
+                {
+                    boreHoleSizes.Add(
+                        new SimulatorBoreHole
+                        {
+                            Depth = (double ) depth,
+                            Diameter = (double) boreHoleSize.HoleSize.GaussianValue.Mean!,
+                            Length  = (double) boreHoleSize.Length.GaussianValue.Mean!  
+                        }
+                    );                    
+                } 
             }
-            foreach (ModelShared.BoreHoleSize bhSize in casingSection.CasingSectionSizeTable)
-            {
-  
-
-                boreHoleSizes.Add(new SimulatorBoreHole
-                    {
-                        Depth = (double ) casingSection.TopDepth.GaussianValue.Mean!,
-                        Diameter = (double) bhSize.HoleSize.GaussianValue.Mean!,
-                        Length  = (double) bhSize.Length.GaussianValue.Mean!  
-                    }
-                );
-                if (bhSize.Length.GaussianValue.Mean != null)
-                    depth += (double) bhSize.Length.GaussianValue.Mean;                
-            }
-
-            this.boreHoleSizes = boreHoleSizes; 
-            boreholeRadius = Vector<double>.Build.Dense(drillString.OuterRadius.Count);
+            BoreHoleSizes = boreHoleSizes;
             DrillStringClearance = Vector<double>.Build.Dense(drillString.OuterRadius.Count);
 
             UpdateWellbore(drillString, lumpedCells);
         }
-
+        
         public void UpdateWellbore(in SimulatorDrillString drillString, in LumpedCells lumpedElement)
         {
             // Wellbore radius calculation
             boreholeRadius = Vector<double>.Build.Dense(drillString.OuterRadius.Count);
-            int idx = 0;
+            int index = 0;
+            double localRadius;
             for (int i = 1; i < lumpedElement.ElementLength.Count(); i++)
             {
-                if (idx < boreHoleSizes.Count)
-                {
-                    if (lumpedElement.ElementLength[i] <= boreHoleSizes[idx].Depth)
-                    {
-                        boreholeRadius[i - 1] = 0.5 * boreHoleSizes[idx].Diameter;
-                    }
-                    else
-                    {
-                        while (idx < boreHoleSizes.Count)
-                        {
-                            idx++;
-                            if (idx < boreHoleSizes.Count && lumpedElement.ElementLength[i] <= boreHoleSizes[idx].Depth)
-                            {
-                                boreholeRadius[i - 1] = boreHoleSizes[idx].Diameter / 2;
-                                break;
-                            }
-                        }
-                    }
-                }
-                if (idx >= boreHoleSizes.Count)
-                {
-                    boreholeRadius[i - 1] = drillString.BitRadius;
-                }
+                // If the element depth is greater than the borehole, go to the next one
+                index += (lumpedElement.ElementLength[i] > BoreHoleSizes[index].Depth) ? 1 : 0; 
+                // Switch between borehole radius and bit radius
+                localRadius = index < BoreHoleSizes.Count ? 0.5 * BoreHoleSizes[index].Diameter : drillString.BitRadius;
+                //Update radius list
+                boreholeRadius[i - 1] = localRadius;            
             }
 
             DrillStringClearance = boreholeRadius - drillString.OuterRadius;
             foreach (int i in drillString.SleeveIndexPosition)
+            {
                 DrillStringClearance[i] = boreholeRadius[i] - drillString.SleeveOuterRadius;
+            }
         }
     }
 }
